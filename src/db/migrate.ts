351c868cef39366f66247753f955f99d773863cb
Misha Hinton <squishah.yt@gmail.com>
2024-01-24 12:02:34 +0300

    feat: :memo: updated tables
    updated ui to reflect tables/functionality

--- src/db/migrate.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/sqlite-proxy";
-import { migrate } from "drizzle-orm/sqlite-proxy/migrator";
-// import Database from "better-sqlite3";
-
-// TODO: Create client and run migration to get files
-
-// const betterSqlite = new Database(":memory:");
-// const db = drizzle(betterSqlite);
-// migrate(db, { migrationsFolder: "drizzle" });
